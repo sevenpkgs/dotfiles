c5912cc9c953139cde32aa1cab92a78188dd11ae
sevenpkgs <171176515+sevenpkgs@users.noreply.github.com>
2025-03-09 20:30:56 +0530

    added script to manage `xidlehook`

--- scripts/autolock.sh
+++ scripts/autolock.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-# Only exported variables can be used within the timer's command.
-export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-
-# Run xidlehook
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
-  --timer 60 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 25%' \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  --timer 10 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; slock' \
+  --timer 120\
+    'brightnessctl s 25%'\
+    'brightnessctl s 50%'\
+  --timer 7\
+    'slock' \
     '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
+  --timer 600 \
     'systemctl suspend' \
     ''
