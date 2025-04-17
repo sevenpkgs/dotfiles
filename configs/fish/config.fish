170a05d4d9afe018da44d29f22e87843b0ccda41
sevenpkgs <171176515+sevenpkgs@users.noreply.github.com>
2025-04-17 23:33:58 +0530

    added func `y.fish`

--- configs/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-
-set fish_greeting
-
-end
--- programs/fish.nix
+++ programs/fish.nix
@@ -5,24 +5,26 @@
   ...
 }: {
   # fish
-  programs.fish.enable = true;
   programs.fish = {
+    enable = true;
     interactiveShellInit = ''
       set fish_greeting
     '';
+    shellAliases = {
+      procuptime = "ps -o etime= -p";
+      ls = "eza";
+      l = "eza -l";
+      la = "eza -la";
+      tree = "eza -T";
+      fdf = "cd ~/.dotfiles && ff ~/.dotfiles";
+      fdfn = "ff ~/.dotfiles";
+    };
   };
-  programs.fish.shellAliases = {
-    procuptime = "ps -o etime= -p";
-    ls = "eza";
-    l = "eza -l";
-    la = "eza -la";
-    tree = "eza -T";
-    fdf = "cd ~/.dotfiles && ff ~/.dotfiles";
-    fdfn = "ff ~/.dotfiles";
-  };
-
   home.sessionPath = [
     "/home/seven/.dotfiles/scripts/"
   ];
+
+  # functions
   home.file.".config/fish/functions/fish_prompt.fish".source = ../configs/fish/functions/fish_prompt.fish;
+  home.file.".config/fish/functions/y.fish".source = ../configs/fish/functions/y.fish;
 }
