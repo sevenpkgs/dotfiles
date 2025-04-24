6ea2b47a5c75fa28000ffac3fe4587f37007339d
sevenpkgs <171176515+sevenpkgs@users.noreply.github.com>
2025-04-24 12:28:20 +0530

    update bat.sh to show capacity below 10%

--- modules/dwm/dwmblocks/bat.sh
+++ modules/dwm/dwmblocks/bat.sh
@@ -3,12 +3,13 @@
 bat="$(cat /sys/class/power_supply/BAT0/capacity)"
 
 case 1 in
-	$((bat >= 85)) ) icon="  " ;;
-	$((bat >= 75)) ) icon="  " ;;
-	$((bat >= 50)) ) icon="  " ;;
-	$((bat >= 25)) ) icon="  " ;;
-	$((bat >= 10)) ) icon="  " ;;
-	* ) echo ' ' && exit ;;
+$((bat >= 85))) icon="  " ;;
+$((bat >= 75))) icon="  " ;;
+$((bat >= 50))) icon="  " ;;
+$((bat >= 25))) icon="  " ;;
+$((bat >= 10))) icon="  " ;;
+$((bat >= 0))) icon="  " ;;
+*) echo ' ' && exit ;;
 esac
 
 echo "$icon$bat%"
